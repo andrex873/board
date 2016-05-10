5d8b35288bd3beccd336b99a3ccc8d0310e1b1ef
Andrés Méndez <andrex873@gmail.com>
2016-05-09 19:06:52 -0500

    Deleted update action.

--- app/Http/Controllers/BoardsController.php
+++ app/Http/Controllers/BoardsController.php
@@ -83,18 +83,6 @@ class BoardsController extends ApiController
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
